d7173e41708c997def652deef76884ce87c71bc0
Sheppard Y <ysf1026@gmail.com>
2015-10-03 14:31:38 +0800

    血条

--- client/src/Player.ts
+++ client/src/Player.ts
@@ -1,4 +1,7 @@
 class Player extends egret.DisplayObjectContainer {
+    private hpBar:eui.ProgressBar;
+    private nameText;
+    
     public constructor(x:number, y:number, color:number, listener) {
         super();
 
@@ -9,5 +12,27 @@ class Player extends egret.DisplayObjectContainer {
         this.addChild(shape);
         shape.touchEnabled = true;
         shape.addEventListener(egret.TouchEvent.TOUCH_TAP, listener, shape);
+        
+        this.initHPBar(x, y, color);
+    }
+    
+    private initHPBar(x:number, y:number, color:number):void {
+        var exml =
+            `<s:ProgressBar xmlns:s="http://ns.egret.com/eui">
+                <s:Skin>
+                    <s:Image id="thumb" width="100%" height="100%" source="resource/thumb.png" scale9Grid="1,1,4,4"/>
+                    <s:Label id="labelDisplay" textColor="${color}" size="20" horizontalCenter="0" verticalCenter="0" percentHeight="100%" percentWidth="100%"/>
+                </s:Skin>
+            </s:ProgressBar>`;
+        var clazz = EXML.parse(exml);
+        this.hpBar = new clazz();
+        this.hpBar.maximum = 9;
+        this.hpBar.minimum = 0;
+        this.hpBar.width = 50;
+        this.hpBar.height = 20;
+        this.hpBar.x = x;
+        this.hpBar.y = y-25;
+        this.addChild(this.hpBar);
+        this.hpBar.value = 9;
     }
 }
--- client/template/runtime/native_require.js
+++ client/template/runtime/native_require.js
@@ -9,6 +9,7 @@ var game_file_list = [
 	"libs/modules/game/game.native.js",
 	"libs/modules/tween/tween.js",
 	"libs/modules/res/res.js",
+	"libs/modules/eui/eui.js",
 	"bin-debug/Battle.js",
 	"bin-debug/LoadingUI.js",
 	"bin-debug/Main.js",
